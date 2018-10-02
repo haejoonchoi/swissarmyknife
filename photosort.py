8c18a9bcf54b2bfa31dbec5f942b1e90df24e0f2
Richard Cook <rcook@rcook.org>
2018-10-02 15:27:37 -0700

    Separate input and output paths

--- photosort.py
+++ photosort.py
@@ -18,39 +18,34 @@ def get_date(path):
             return None
         return datetime.datetime.strptime(str(value), "%Y:%m:%d %H:%M:%S")
 
-def rename(source_path):
-    d = get_date(source_path)
+def rename(input_path, output_dir):
+    input_file_name = os.path.basename(input_path)
+    d = get_date(input_path)
     if d is None:
         return
 
-    source_dir = os.path.dirname(source_path)
-    source_file_name = os.path.basename(source_path)
+    target_file_name = "{0:04d}{1:02d}{2:02d}-{3}".format(d.year, d.month, d.day, input_file_name)
+    target_subdir = os.path.join("{0:04d}".format(d.year), "{0:04d}-{1:02d}".format(d.year, d.month))
+    target_dir = os.path.join(output_dir, target_subdir)
+    target_path = os.path.join(target_dir, target_file_name)
 
-    target_file_name = "{0:04d}{1:02d}{2:02d}-{3}".format(d.year, d.month, d.day, source_file_name)
-    target_dir = os.path.join(
-        source_dir,
-        os.path.join("{0:04d}".format(d.year), "{0:04d}-{1:02d}".format(d.year, d.month)))
-    target_path = os.path.join(
-        target_dir,
-        target_file_name)
+    print("{} -> {}".format(input_path, target_path))
 
-    print(target_path)
-
-def scan(start_dir):
-    for file_name in os.listdir(start_dir):
+def rename_all(input_dir, output_dir):
+    for file_name in os.listdir(input_dir):
         _, ext = os.path.splitext(file_name)
         if ext.lower() in EXTS:
-            source_path = os.path.join(start_dir, file_name)
-            rename(source_path)
+            rename(os.path.join(input_dir, file_name), output_dir)
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
     parser = argparse.ArgumentParser(description="Create sorted directory structure for photos based on metadata")
-    parser.add_argument("start_dir", metavar="STARTDIR", type=os.path.abspath)
+    parser.add_argument("input_dir", metavar="INPUTDIR", type=os.path.abspath)
+    parser.add_argument("output_dir", metavar="OUTPUTDIR", type=os.path.abspath)
     args = parser.parse_args(argv)
-    scan(args.start_dir)
+    rename_all(args.input_dir, args.output_dir)
 
 if __name__ == "__main__":
     main()
