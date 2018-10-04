34ab35bdcf1a266d410122f36e5ad09897967ef2
Richard Cook <rcook@rcook.org>
2018-10-03 20:17:19 -0700

    Move SHA1 function into shared.py and fix bug

--- shared.py
+++ shared.py
@@ -2,10 +2,33 @@
 # Shared functions
 # https://github.com/rcook/pyfileutils
 
+import hashlib
+import os
+
 GITHUB_URL = "https://github.com/rcook/pyfileutils"
+
+BLOCK_SIZE = 1024
 
 GIB_THRESHOLD = 1024 * 1024 * 1024
 MIB_THRESHOLD = 1024 * 1024
+
+def compute_sha1(path, partial=False, include_file_size=True):
+    file_size = os.stat(path).st_size
+    if partial:
+        block_count = 1
+    else:
+        block_count = (file_size / BLOCK_SIZE) + (1 if (file_size % BLOCK_SIZE) > 0 else 0)
+
+    m = hashlib.sha1()
+    with open(path, "rb") as f:
+        for _ in range(0, block_count):
+            m.update(f.read(BLOCK_SIZE))
+
+    d = m.hexdigest()
+    if include_file_size:
+        return "{}:{}".format(file_size, d)
+    else:
+        return d
 
 def add_switch_with_inverse(parser, name, default, help=None, inverse_help=None):
     group = parser.add_mutually_exclusive_group()
